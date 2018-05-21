ae257074d32d93399aa188032eab74f48732d8e0
NoJohns <viktoraxillus@gmail.com>
2018-05-21 20:29:28 +0200

    first test case for user story 6

--- tests/viaxTest.java
+++ tests/viaxTest.java
@@ -165,4 +165,29 @@ public class viaxTest {
 
     }
 
+
+    @Test
+    public void spareTest() {
+        Game game = new Game();
+        int nrOfFrames = 10;
+        Frame[] playedFrames = new Frame[nrOfFrames];
+
+        playedFrames[0] = new Frame(1, 9);
+        playedFrames[1] = new Frame(3, 6);
+        playedFrames[2] = new Frame(7, 2);
+        playedFrames[3] = new Frame(3, 6);
+        playedFrames[4] = new Frame(4, 4);
+        playedFrames[5] = new Frame(5, 3);
+        playedFrames[6] = new Frame(3, 3);
+        playedFrames[7] = new Frame(4, 5);
+        playedFrames[8] = new Frame(8, 1);
+        playedFrames[9] = new Frame(2, 6);
+
+        game.setFrames(playedFrames, nrOfFrames);
+
+        collector.checkThat("Testing getScore for a game containing a spare (should be 88)",
+                game.getScore(), equalTo(88));
+
+    }
+
 }
