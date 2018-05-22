9bfb94391c017b0558b8f6ce7b9c95a1d339b3f5
NoJohns <viktoraxillus@gmail.com>
2018-05-22 08:00:27 +0200

    first test for user story 9 (it is passed)

--- tests/viaxTest.java
+++ tests/viaxTest.java
@@ -218,6 +218,31 @@ public class viaxTest {
 
 
     }
+    @Test
+    public void mulitpleSpareTest() {
+        Game game = new Game();
+        int nrOfFrames = 10;
+        Frame[] playedFrames = new Frame[nrOfFrames];
+
+        playedFrames[9] = new Frame(8, 2);
+        playedFrames[8] = new Frame(5, 5);
+        playedFrames[7] = new Frame(7, 2);
+        playedFrames[6] = new Frame(3, 6);
+        playedFrames[5] = new Frame(4, 4);
+        playedFrames[4] = new Frame(5, 3);
+        playedFrames[3] = new Frame(3, 3);
+        playedFrames[2] = new Frame(4, 5);
+        playedFrames[1] = new Frame(8, 1);
+        playedFrames[0] = new Frame(2, 6);
+
+        game.setFrames(playedFrames, nrOfFrames);
+
+        collector.checkThat("Testing getScore for a gaming containing a strike followed by a spare",
+                game.getScore(), equalTo(98));
+
+
+    }
+
 
 
 
